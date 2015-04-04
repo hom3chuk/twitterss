d27d2e1cf86ac85d2a0d8cccc133bc8ca0936c5f
Eugene Chekan <e.chekan@flamp.ru>
2015-04-05 04:03:51 +0600

    wow such opensurs

---
+++ src/Poster.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hom3chuk
+ * Date: 02.04.15
+ * Time: 22:11
+ */
+
+namespace hom3chuk\twitterss;
+
+
+class Poster {
+
+}
---
+++ twitterss.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hom3chuk
+ * Date: 02.04.15
+ * Time: 23:20
+ */
